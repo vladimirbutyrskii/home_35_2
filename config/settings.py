dab9ef80218af91bda84cbc8b4972d12ff2b3ccf
vladimirbutyrskii <vlaver@mail.ru>
2026-05-13 18:46:15 +0300

    error correction after black

--- config/settings.py
+++ config/settings.py
@@ -198,11 +198,10 @@ if CACHE_ENABLED:
 
 CACHE_TTL = 60 * 15  # 15 минут
 
-if 'test' in sys.argv:
+if "test" in sys.argv:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
         }
     }
-
