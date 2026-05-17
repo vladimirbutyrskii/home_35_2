64605a47768f9045994384ade4e592654c0ec0b6
vladimirbutyrskii <vlaver@mail.ru>
2026-05-17 15:21:10 +0300

    edining settings.py and .env.template

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key-for-dev")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
